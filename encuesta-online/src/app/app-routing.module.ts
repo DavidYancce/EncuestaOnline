80d7ed93a22a40032f0df1b3ed47a7a020312c6c
DavidYQ <david.yanccehuallpa.q@uni.pe>
2021-11-18 12:28:29 -0500

    Componentes

--- encuesta-online/src/app/app-routing.module.ts
+++ encuesta-online/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CuestionarioComponent } from './encuesta/cuestionario/cuestionario.component';
+import {LoginComponent } from './encuesta/login/login.component';
+import {InicioComponent} from './encuesta/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cuestionario', component: CuestionarioComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- encuesta-online/src/app/app.module.ts
+++ encuesta-online/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CuestionarioComponent } from './encuesta/cuestionario/cuestionario.component';
+import { InicioComponent } from './encuesta/inicio/inicio.component';
+import { LoginComponent } from './encuesta/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuestionarioComponent,
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
