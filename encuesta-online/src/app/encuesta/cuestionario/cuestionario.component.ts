febeb650515e8115c8a7d9b8f6d6812aa4ce6ce1
DavidYQ <david.yanccehuallpa.q@uni.pe>
2021-11-20 12:17:46 -0500

    insercion api

--- encuesta-online/src/app/encuesta/cuestionario/cuestionario.component.ts
+++ encuesta-online/src/app/encuesta/cuestionario/cuestionario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
 import { CuestionarioServiceService } from 'src/app/services/cuestionario-service.service';
 
 @Component({
@@ -8,17 +9,25 @@ import { CuestionarioServiceService } from 'src/app/services/cuestionario-servic
 })
 export class CuestionarioComponent implements OnInit {
 
-  constructor(private _cuestionarioService : CuestionarioServiceService) {
-    
-   }
+  constructor(private _cuestionarioService: CuestionarioServiceService) {
+
+  }
 
   ngOnInit(): void {
-    this._cuestionarioService.getcomoDicen().subscribe(data=>{
-      for (let i = 0; i < data.length;i++) {
+    this._cuestionarioService.getcomoDicen().subscribe(data => {
+      for (let i = 0; i < data.length; i++) {
         this.palabras.push(data[i].descripcion)
       }
     });
   }
-  palabras: string[] = [];
-  
+  palabras: string[] = ['opcion 1', 'opcode 2'];
+  palabrasSeleccionadas: any[] = [];
+
+  onGroupsChange(options: MatListOption[]) {
+    for(var option of  options) {
+      console.log(option.value)
+      this.palabrasSeleccionadas.push(option.value)
+      console.log(this.palabrasSeleccionadas)
+    }
+  }
 }
